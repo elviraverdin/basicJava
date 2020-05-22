c837f39a1d6cca2d781748af7e59e8ef446be605
elviraverdin <elvira.verdin@astrazeneca.com>
2020-05-21 19:49:14 -0500

    Cancion de los Elefantes Elvira 2105

---
+++ src/iLoops/cancionElefantes.java
@@ -0,0 +1,17 @@
+package iLoops;
+
+public class cancionElefantes {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i<=100; i++)
+        {
+            if (i == 1)
+            {
+                System.out.println(i+" se columpiaba sobre la tela de una araña como veia que resistia fue a llamar a otro elefante \n" );
+            }
+            else
+                System.out.println(i+" se columpiaban sobre la tela de una araña como veian que resistian fueron a llamar a otro elefante \n" );
+        }
+    }
+}
